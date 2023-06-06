a0d7483dacf199e6f35950d9b61b5ed34cd1804e
usuario <vicenlld@gmail.com>
2023-06-06 10:30:24 +0200

    Menu +

---
+++ app/src/main/java/com/example/pfg_ecommerce/MainNavegation.kt
@@ -0,0 +1,35 @@
+package com.example.pfg_ecommerce
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.pfg_ecommerce.databinding.ActivityMainNavegationBinding
+
+class MainNavegation : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainNavegationBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainNavegationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main_navegation)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+}
